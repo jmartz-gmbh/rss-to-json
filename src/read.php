d4f49560e543f321c0de71675a1e842943b05d33
Jonathan Martz <github@jmartz.de>
2021-02-19 16:05:26 +0100

    initial commit

---
+++ src/read.php
@@ -0,0 +1,19 @@
+<?php
+
+// create curl resource
+$ch = curl_init();
+
+// set url
+curl_setopt($ch, CURLOPT_URL, 'https://dev98.de/feed/');
+
+//return the transfer as a string
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// $output contains the output string
+$output = curl_exec($ch);
+$xml = simplexml_load_string($output);
+$json = json_encode($xml);
+echo $json;
+
+// close curl resource to free up system resources
+curl_close($ch);
